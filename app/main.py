3c7e3ac09f30d90a5f2a05635a9129a7fa18a1ee
Ben Garski <ben.garski@outlook.com>
2023-01-27 11:03:36 -0700

    firt commit with base files

---
+++ app/main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+import pandas as pd
+from google.cloud import bigquery
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def get_workout():
+    if request.method == "POST":
+        client = bigquery.Client()
+        table_id = "flask-workout.workout.exercises"
+        query = f"""
+            SELECT exercise, category
+            FROM {table_id}
+            WHERE
+            type = 'Kettlebell';  
+            """
+        query_job = client.query(query)
+        df = query_job.result().to_dataframe()
+        return df.head()
+    else:
+        return render_template("base.html")
