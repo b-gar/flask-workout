2d7ed5b90482b6694a3aaf97b78583e016a86aed
Ben Garski <ben.garski@outlook.com>
2023-01-27 11:51:48 -0700

    checking to see if bigquery client is authenticating properly

--- app/main.py
+++ app/main.py
@@ -1,25 +1,24 @@
 from flask import Flask, render_template, request
 import pandas as pd
 from google.cloud import bigquery
+import os
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def get_workout():
     if request.method == "POST":
-        try:
-            client = bigquery.Client()
-            table_id = "flask-workout.workout.exercises"
-            query = f"""
-                SELECT exercise, category
-                FROM {table_id}
-                WHERE
-                type = 'Kettlebell';  
-                """
-            query_job = client.query(query)
-            df = query_job.result().to_dataframe()
-            return df.head()
-        except Exception as e:
-            print(e)
+        client = bigquery.Client()
+        return client
+        # table_id = "flask-workout.workout.exercises"
+        # query = f"""
+        #     SELECT exercise, category
+        #     FROM {table_id}
+        #     WHERE
+        #     type = 'Kettlebell';
+        #     """
+        # query_job = client.query(query)
+        # df = query_job.result().to_dataframe()
+        # return df.head()
     else:
         return render_template("base.html")
