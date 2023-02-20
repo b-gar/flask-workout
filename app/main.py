316aa71b74cde607407ce0064a41f1bde5e1d006
Ben Garski <ben.garski@outlook.com>
2023-02-20 14:18:17 -0700

    Added function to get dynamo db table to df

--- app/main.py
+++ app/main.py
@@ -1,13 +1,24 @@
 from flask import Flask, render_template, request
 import pandas as pd
-from utils import aws_authenticate
+from utils import aws_authenticate, dynamo_to_df, num_exercises, num_seconds
 
 app = Flask(__name__)
 
+table = aws_authenticate("workout-exercises")
+df = dynamo_to_df(table)
+
 @app.route("/", methods=["GET", "POST"])
-def get_workout():
+def home():
+    return render_template("home.html")
+
+@app.route("/create", methods=["GET", "POST"])
+def create():
     if request.method == "POST":
-        table = aws_authenticate("workout-exercises")
-        return table.get_item(Key={"key": "key1"})
+        n_exercises = num_exercises(request.form.get("difficulty"), request.form.get("duration"))
+        n_seconds = num_seconds(request.form.get("difficulty"))
+        return render_template("create.html", difficulty=n_exercises, equipment=n_seconds, duration="")
     else:
-        return render_template("base.html")
+        return render_template("create.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,7 @@
 import boto3
 import os
+from math import floor
+import pandas as pd
 
 def aws_authenticate(table_name):
     dynamo = boto3.resource(
@@ -10,3 +12,35 @@ def aws_authenticate(table_name):
     )
     table = dynamo.Table(table_name)
     return table
+
+def dynamo_to_df(table):
+    results = []
+    last_evaluated_key = None
+    while True:
+        if last_evaluated_key:
+            response = table.scan(ExclusiveStartKey=last_evaluated_key)
+        else:
+            response = table.scan()
+        last_evaluated_key = response.get("LastEvaluatedKey")
+
+        results.extend(response["Items"])
+
+        if not last_evaluated_key:
+            break
+    return pd.DataFrame(results)
+
+def num_exercises(difficulty_input, duration_input):
+    if difficulty_input == "Easy":
+        return floor(int(duration_input) / 2.75)
+    elif difficulty_input == "Moderate":
+        return floor(int(duration_input) / 3.42)
+    else:
+        return floor(int(duration_input) / 4.42)
+
+def num_seconds(difficulty_input):
+    if difficulty_input == "Easy":
+        return 20
+    elif difficulty_input == "Moderate":
+        return 30
+    else:
+        return 45
