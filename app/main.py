368b784269d561dd9a00224526c01f01b40f8c1f
Ben Garski <ben.garski@outlook.com>
2023-02-23 13:49:26 -0700

    fixed download table button

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, request
 import pandas as pd
 from utils import aws_authenticate, dynamo_to_df, num_exercises, filter_exercises, create_table
+from pathlib import Path
 
 app = Flask(__name__)
+
+file_path = Path.cwd().joinpath("static")
+file_name = "exercises.csv"
 
 table = aws_authenticate("workout-exercises")
 df = dynamo_to_df(table)
@@ -17,6 +21,7 @@ def create():
         n_exercises = num_exercises(request.form.get("difficulty"), request.form.get("duration"))
         df_filtered = filter_exercises(df, request.form.get("equipment"), n_exercises)
         df_final = create_table(df_filtered, request.form.get("difficulty"))
+        df_final.to_csv(file_path / file_name, index=False)
         return render_template("create.html", df=df_final)
     else:
         return render_template("create.html", df=pd.DataFrame())
